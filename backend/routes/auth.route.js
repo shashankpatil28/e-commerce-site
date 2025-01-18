7307c07464d8abb592e9e24df93d4636605e887e
shashankpatil28 <patilshashank2003@gmail.com>
2025-01-18 23:42:05 +0530

    project finished

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,16 @@
+import express from "express"
+
+const router = express.Router();
+
+import {
+    signup, login, logout, refreshToken, getProfile
+} from "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+router.post("/signup", signup)
+router.post("/login", login)
+router.post("/logout", logout)
+router.post("/refresh-token", refreshToken)
+router.get("/profile", protectRoute, getProfile)
+
+export default router
